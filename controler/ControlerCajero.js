e5f9ea5485acf1b7f14feacb22c7c9a418806da4
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-11-25 13:07:40 -0500

    cambios adrian

--- controler/ControlerCajero.js
+++ controler/ControlerCajero.js
@@ -1,7 +1,9 @@
 import { ConsultaSaldo } from "../model/ModelConsultaSaldo.js";
 console.log(ConsultaSaldo)
 
+document.getElementById('panel').style.display = 'block';
 
+document.getElementById('RetirarDinero').addEventListener("click", retirarDinero)
 let ConsultarSaldo = document.getElementById('ConsultarSaldo')
 ConsultarSaldo.addEventListener('click', function () {
     document.getElementById('sectionConsultarSaldo').innerHTML = ''
@@ -20,6 +22,7 @@ ConsultarSaldo.addEventListener('click', function () {
         movimiento.textContent = Csaldo.movimiento
         fecha.textContent = Csaldo.fecha
         card.classList.add('card')
+        card.id = "card"
         card.append(usuario, saldo, movimiento, fecha)
         document.getElementById('sectionConsultarSaldo').append(card)
 
@@ -30,63 +33,73 @@ ConsultarSaldo.addEventListener('click', function () {
 })
 
 
- 
 
-    saldo = Number(prompt("Ingrese su saldo actual: "));
 
-    
-    function mostrarSaldoActual() {
-        console.log("Su saldo es: " + saldo);
+let saldo = Number(prompt("Ingrese su saldo actual: "));
+
+
+function mostrarSaldoActual() {
+    console.log("Su saldo es: " + saldo);
+}
+function retirarDinero() {
+    let card = document.getElementsByClassName("card")[0];
+    console.log(card)
+    let input = document.createElement("input");
+    input.type = "number";
+    input.placeholder = "Cantidad a retirar";
+    card.appendChild(input);
+
+}
+
+function retirar() {
+    let valorRetiro = Number(prompt("Ingrese el valor a retirar: "));
+    if (valorRetiro <= saldo) {
+        saldo -= valorRetiro;
+        console.log("Se retiraron " + valorRetiro + " pesos. Su nuevo saldo es: " + saldo);
+    } else {
+        console.log("Fondos insuficientes");
     }
-        function retirarDinero() {
-            let valorRetiro = Number(prompt("Ingrese el valor a retirar: "));
-            if (valorRetiro <= saldo) {
-                saldo -= valorRetiro;
-                console.log("Se retiraron " + valorRetiro + " pesos. Su nuevo saldo es: " + saldo);
-            } else {
-                console.log("Fondos insuficientes");
-            }
-        }
-        ConsultaSaldo.map((Csaldo) => {
-            let saldo = document.createElement('p');
-            let movimiento = document.createElement('p');
-            let fecha = document.createElement('p');
-            let card = document.createElement('article');
-            saldo.textContent = Csaldo.saldo;
-            movimiento.textContent = Csaldo.movimiento;
-            fecha.textContent = Csaldo.fecha;
-            card.classList.add('card');
-            card.append(saldo, movimiento, fecha);
-            document.getElementById('sectionRetirarDinero').append(card);
-            mostrarSaldoActual();
-            retirarDinero();
-        });
-    
-        
-        // function saldoAcutual() {
-        // console.log("su saldo es: " + saldo)
-        //  }
-        //  function RetirarDinero() {
-        //   let valorRetiro = Number(prompt("ingrese el valor a retirar: "))
-        //   if(valorRetiro <= saldo){
-        //   saldo -= valorRetiro
-        // console.log("Su nuevo saldo es: " + saldo)
-        //  }else{
-        //   console.log("Fondos insuficientes")
-        //  }
-        //  }
-        //  let opcion = Number(prompt("seleccione: \n1- consultar saldo\n2- retirar dinero"))
-        //  switch (opcion) {
-        //   case 1: 
-        //   ConsultarSaldo()
-        //     break;
-        //  case 2: 
-        //  retirarDinero()
-        //  break; 
-        //  default:
-        //   console.log("esta opción no existe")
-        //     break;
-        //  }
+}
+ConsultaSaldo.map((Csaldo) => {
+    let saldo = document.createElement('p');
+    let movimiento = document.createElement('p');
+    let fecha = document.createElement('p');
+    let card = document.createElement('article');
+    saldo.textContent = Csaldo.saldo;
+    movimiento.textContent = Csaldo.movimiento;
+    fecha.textContent = Csaldo.fecha;
+    card.classList.add('card');
+    card.append(saldo, movimiento, fecha);
+    document.getElementById('sectionRetirarDinero').append(card);
+    mostrarSaldoActual();
+    retirarDinero();
+});
+
+
+// function saldoAcutual() {
+// console.log("su saldo es: " + saldo)
+//  }
+//  function RetirarDinero() {
+//   let valorRetiro = Number(prompt("ingrese el valor a retirar: "))
+//   if(valorRetiro <= saldo){
+//   saldo -= valorRetiro
+// console.log("Su nuevo saldo es: " + saldo)
+//  }else{
+//   console.log("Fondos insuficientes")
+//  }
+//  }
+//  let opcion = Number(prompt("seleccione: \n1- consultar saldo\n2- retirar dinero"))
+//  switch (opcion) {
+//   case 1: 
+//   ConsultarSaldo()
+//     break;
+//  case 2: 
+//  retirarDinero()
+//  break; 
+//  default:
+//   console.log("esta opción no existe")
+//     break;
+//  }
 
 
 
@@ -113,7 +126,8 @@ TransferirACuentasInscritas.addEventListener('click', function (RetirarDinero) {
         saldo.textContent = Csaldo.saldo
         movimiento.textContent = Csaldo.movimiento
         fecha.textContent = Csaldo.fecha
-        card.classList.add('card')
+        card.id = "1";
+        card.classList.add('1card')
         card.append(usuario, saldo, movimiento, fecha)
         document.getElementById('sectionTransferirACuentasInscritas').append(card)
 
--- controler/ControlerLogin.js
+++ controler/ControlerLogin.js
@@ -3,23 +3,24 @@ console.log(ConsultaSaldo)
 
 let btnIniciar = document.getElementById('btnIniciar')
 btnIniciar.addEventListener('click', function () {
-    console.log('iniciaste section')
+    console.log('iniciaste sesion')
+    window.location.href = "/cuenta.html";
     // let usuario = document.getElementById('usuario').value;
     // if (usuario == 'lucas') {
     //     let contrasena = document.getElementById('contrasena').value;
     //     if (contrasena == '1234') {
     //         console.log(usuario, contrasena )
-            document.getElementById('spinner').style.display = 'block';
+    document.getElementById('spinner').style.display = 'block';
 
-            setTimeout(() => {
-                document.getElementById('spinner').style.display = 'none';
-                document.getElementById('login').style.display = 'none';
-                document.getElementById('panel').style.display = 'block';
-            },
-            // 1000
-            );
+    setTimeout(() => {
+        document.getElementById('spinner').style.display = 'none';
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('panel').style.display = 'block';
+    },
+        // 1000
+    );
 
-           
+
     //     } else {
     //         console.log('Datos incorrectos')
     //     }
@@ -36,7 +37,7 @@ cerrarSeccion.addEventListener('click', () => {
         document.getElementById('spinner').style.display = 'none';
         document.getElementById('login').style.display = 'block';
         document.getElementById('panel').style.display = 'none';
-    },1000);
+    }, 1000);
 })
 
 
